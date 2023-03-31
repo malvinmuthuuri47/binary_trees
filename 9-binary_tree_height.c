7f3c352a415c2c13ccb10705e069bdccb2cf97ee
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-03-31 18:31:48 +0300

    Corrected task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right, left, height;
 
-	if (!tree ||(!tree->left && !tree->right))
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	left = binary_tree_height(tree->left) + 1;
